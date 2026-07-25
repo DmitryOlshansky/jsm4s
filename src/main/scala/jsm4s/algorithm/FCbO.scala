1e14cf0170c73e51308d76c8adc862161c866388
Dmitry Olshansky <dmitry@olshansky.me>
2026-07-25 23:04:25 +0300

    Alter implementation of (P)FCbO

--- src/main/scala/jsm4s/algorithm/FCbO.scala
+++ src/main/scala/jsm4s/algorithm/FCbO.scala
@@ -3,33 +3,34 @@ package jsm4s.algorithm
 import java.util.concurrent.{ForkJoinPool, TimeUnit}
 
 import jsm4s.ds.FcaSet
+import java.util.concurrent.atomic.AtomicInteger
 
 abstract class GenericFCbO(context: Context)
-  extends Algorithm(context) with QueueAlgorithm[(FcaSet, FcaSet, Int, Array[FcaSet], Int)] {
+  extends Algorithm(context) with QueueAlgorithm[(FcaSet, FcaSet, Int, Array[FcaSet])] {
 
   var recDepth = 0
 
-  def method(A: FcaSet, B: FcaSet, y: Int, errors: Array[FcaSet], N: Int): Unit = {
-    val q = Array.ofDim[(FcaSet, FcaSet, Int, Array[FcaSet], Int)](attributes - y)
+  def method(A: FcaSet, B: FcaSet, y: Int, errors: Array[FcaSet]): Unit = {
+    val q = Array.ofDim[(FcaSet, FcaSet, Int, Array[FcaSet])](attributes - y)
     var top = 0
     output(A, B)
-    val M = N + attributes
+    val nextErrors = Array.ofDim[FcaSet](attributes)
     var j = y
     while (j < attributes) {
-      errors(M + j) = errors(N + j)
+      nextErrors(j) = errors(j)
       if (!B.contains(j)) {
-        if (errors(N + j).subsetOf(B, j)) {
+        if (errors(j).subsetOf(B, j)) {
           val ret = closeConcept(A, j)
           stats.onClosure()
           if (ret._1) {
             val C = ret._2
             val D = ret._3
             if (B.equalUpTo(D, j)) {
-              q(top) = (C, D, j + 1, errors, M)
+              q(top) = (C, D, j + 1, nextErrors)
               top += 1
             }
             else {
-              errors(M + j) = D
+              nextErrors(j) = D
               stats.onCanonicalTestFailure()
             }
           }
@@ -49,26 +50,35 @@ abstract class GenericFCbO(context: Context)
   override def perform = {
     val A = ext.full
     val B = rows.fold(int.full)((a, b) => a & b) // full intersection
-    val implied = Array.ofDim[FcaSet]((attributes + 1) * attributes)
-    for (i <- 0 until ((attributes + 1) * attributes)) implied(i) = int.empty
-    method(A, B, 0, implied, 0)
+    val implied = Array.ofDim[FcaSet](attributes)
+    for (i <- 0 until attributes) implied(i) = int.empty
+    method(A, B, 0, implied)
   }
 }
 
 class FCbO(context: Context)  extends GenericFCbO(context) {
-  def processQueue(x: (FcaSet, FcaSet, Int, Array[FcaSet], Int)): Unit = {
-    method(x._1, x._2, x._3, x._4, x._5)
+  def processQueue(x: (FcaSet, FcaSet, Int, Array[FcaSet])): Unit = {
+    method(x._1, x._2, x._3, x._4)
   }
 }
 
-class PFCbO(context: Context, threads: Int) extends GenericFCbO(context)  {
-
+class PFCbO(context: Context, threads: Int) extends GenericFCbO(context)  { 
   private val pool = if (threads == 0) ForkJoinPool.commonPool else new ForkJoinPool(threads)
+  private val cores = pool.getParallelism()
+  private val submitted = new AtomicInteger(0)
 
-  override def processQueue(tup: (FcaSet,FcaSet,Int,Array[FcaSet], Int)) = {
-    pool.submit(new Runnable {
-      override def run(): Unit = method(tup._1, tup._2, tup._3, tup._4, tup._5)
-    })
+  override def processQueue(tup: (FcaSet,FcaSet,Int,Array[FcaSet])) = {
+    if (submitted.get() < cores * 4) {
+      submitted.incrementAndGet()
+      pool.submit(new Runnable {
+        override def run(): Unit = {
+          submitted.decrementAndGet()
+          method(tup._1, tup._2, tup._3, tup._4)
+        }
+      })
+    } else {
+      method(tup._1, tup._2, tup._3, tup._4)
+    }
   }
 
   override def perform = {
